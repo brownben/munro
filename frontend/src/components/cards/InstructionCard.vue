a4aae0f36c3af6acc5ea3c2b469cb3370c599068
brownben <9870007+brownben@users.noreply.github.com>
2020-08-03 19:12:38 +0100

    Docs - Upload Instructions
    Update Upload instructions to be clearer, to account for changes to upload and update screenshots

--- frontend/src/components/cards/InstructionCard.vue
+++ frontend/src/components/cards/InstructionCard.vue
@@ -1,18 +1,20 @@
 <template>
   <section class="col-span-2 card">
-    <p class="text-lg font-heading">
-      <span
-        class="inline-flex items-center justify-center w-8 h-8 mr-2 font-bold text-center align-middle border-2 rounded-full border-main text-main font-heading font-lg"
+    <h2 class="mb-2 text-2xl text-center font-heading">
+      <p
+        class="block w-8 h-8 mx-auto mb-1 text-lg font-bold leading-8 text-center align-middle border-2 rounded-full md:inline-block md:mr-3 border-main text-main font-heading"
       >
         {{ number }}
-      </span>
-      <span class="align-middle">
-        <slot name="instruction" />
-      </span>
-    </p>
+      </p>
+      <slot name="title">
+        {{ title }}
+      </slot>
+    </h2>
 
-    <div class="text-opacity-75 text-main-900">
-      <slot name="body" />
+    <div
+      class="w-full mx-auto text-lg leading-8 text-opacity-75 text-main-900 body"
+    >
+      <slot name="default" />
     </div>
   </section>
 </template>
@@ -21,24 +23,29 @@
 export default {
   props: {
     number: { type: Number, default: 1 },
+    title: { type: String, default: '' },
   },
 }
 </script>
 
 <style lang="postcss" scoped>
-img {
-  @apply mt-2;
+.body p {
+  @apply mb-1 leading-6;
+
+  &:last-of-type {
+    @apply mb-2;
+  }
 }
 
-p {
-  @apply my-2;
-}
-
-img {
+.body img {
   @apply w-10/12 m-auto mt-6;
+
+  @screen lg {
+    @apply w-2/3;
+  }
 }
 
-code {
-  @apply mt-4 font-mono bg-main-100 text-main-900 text-opacity-100 p-3 w-full block;
+.body code {
+  @apply my-4 font-mono bg-main-100 text-main-900 text-opacity-100 px-3 py-2 w-full block rounded-shape font-normal;
 }
 </style>
--- frontend/src/views/UploadInstructions.vue
+++ frontend/src/views/UploadInstructions.vue
@@ -8,100 +8,92 @@
         meta: { name: 'robots', content: 'all' },
       }"
       title="Munro - Upload Instructions"
-      description="Instrcutions for how to upload results to Munro, the Fast and Easy Results System for Orienteering Leagues. A simple way to calculate the results for orienteering leagues, with search and sort features"
+      description="Instructions for how to upload results to Munro, the Fast and Easy Results System for Orienteering Leagues. A simple way to calculate the results for orienteering leagues, with search and sort features"
       url="https://munro-leagues.herokuapp.com/upload/instructions"
     />
     <InstructionCard :number="1">
-      <template #instruction>
+      <template #title>
         Go to
         <router-link to="/upload/file" class="link text-main-700"
           >https://munro-leagues.herokuapp.com/upload/file</router-link
         >
       </template>
-      <template #body>
+      <template #default>
         <img
-          src="@/assets/images/UploadInstructions-EmptyFormNew.png"
+          src="@/assets/images/UploadInstructions-Blank.png"
           alt="Empty Upload Form"
         />
       </template>
     </InstructionCard>
 
-    <InstructionCard :number="2">
-      <template #instruction>
-        Enter the Event ID and the 20 digit Upload Key Provided
-      </template>
-      <template #body>
-        <p>
-          The event name should now be displayed. You can get the Event ID and
-          Upload Key from your league coordinator or by emailing
-          munro.leagues@gmail.com The Event ID will be the the form
-          LeagueNameEventNameYYYY-MM-DD with all spaces removed
-        </p>
-        <img
-          src="@/assets/images/UploadInstructions-KeysEnteredNew.png"
-          alt="Upload Form with Keys Entered"
-        />
-      </template>
+    <InstructionCard
+      :number="2"
+      title="Enter the Event ID and the 20 digit Upload Key Provided"
+    >
+      <p>
+        The event name should now be displayed.
+      </p>
+      <p>
+        The Event ID will be the the form LeagueNameEventNameYYYY-MM-DD with all
+        spaces removed.
+      </p>
+      <p>
+        You can get the Event ID and Upload Key from your league coordinator or
+        by emailing munro.leagues@gmail.com
+      </p>
+      <img
+        src="@/assets/images/UploadInstructions-EventDetails.png"
+        alt="Upload Form with Event ID and Upload Key Included"
+      />
     </InstructionCard>
 
-    <InstructionCard :number="3">
-      <template #instruction>
-        Select Your Results File
-      </template>
-      <template #body>
-        <p>
-          This file can be an OE CSV File or SITiming Processable CSV file with
-          semi-colon or comma delimiter. Or another semicolon/ comma delimited
-          CSV file with the following headings, these headings can be in any
-          order:
-        </p>
-        <code
-          >Name; Age Class; Club; Course; Time; Position; Noncompetitive;
-          Status;
-        </code>
-      </template>
+    <InstructionCard :number="3" title="Select Your Results File">
+      <p>
+        Munro accepts any CSV results file in a sensible format, it has been
+        tested with OE CSV files and SITiming Processable CSV files.
+      </p>
+      <p>
+        It can be semicolon or comma delimited and should have the following
+        headings (they can be in any order):
+      </p>
+      <code>
+        Name; Age Class; Club; Course; Time; Position; NonCompetitive; Status;
+      </code>
+      <p>
+        Age Class, Club, Position and NonCompetitive are optional, but are
+        highly recommended to make sure the matching is done correctly
+      </p>
     </InstructionCard>
 
-    <InstructionCard :number="4">
-      <template #instruction>
-        The `Upload File` Button Should Now Appear
-      </template>
-      <template #body>
-        <p>
-          If you are re-uploading results tick the 'Overwrite Existing Results'
-          checkbox
-        </p>
-        <img
-          src="@/assets/images/UploadInstructions-ButtonShowingNew.png"
-          alt="Upload Form with Upload Button Visible"
-        />
-      </template>
-    </InstructionCard>
-
-    <InstructionCard :number="5">
-      <template #instruction>
+    <InstructionCard :number="4" title="Enter Other Results Information">
+      <p>
         Enter the URLs of the Results Uploaded on Other Websites eg. HTML
         Results/ Routegadget/ Winsplits.
-      </template>
-      <template #body>
-        <p>
-          If you forget, or need to change these URLs please contact your league
-          admin or munro.leagues@gmail.com who can update them
-        </p>
-      </template>
+      </p>
+      <p>
+        If you forget, or need to change these URLs please contact your league
+        admin or munro.leagues@gmail.com who can update them
+      </p>
+      <img
+        src="@/assets/images/UploadInstructions-Complete.png"
+        alt="Upload Form with Completeted with Upload Button Showing"
+      />
     </InstructionCard>
 
-    <InstructionCard :number="6">
-      <template #instruction>
-        Click Upload
-      </template>
-      <template #body>
-        <p>
-          A message should appear saying that the data has been sent. Then a
-          message saying that the results upload is successful should appear and
-          you will be redirected to the league page
-        </p>
-      </template>
+    <InstructionCard :number="5" title=" Confirm if Replacing Results">
+      <p>
+        If results already exist a 'Overwrite Existing Results' checkbox will
+        appear, to confirm you wish to replace the exisiting results, tick the
+        box
+      </p>
+    </InstructionCard>
+
+    <InstructionCard :number="6" title="Click `Upload File`">
+      <p>
+        A message should appear saying that the data has been sent. Then a
+        message saying that the results upload is successful should appear and
+        you will be redirected to the results table
+      </p>
     </InstructionCard>
 
     <div class="col-span-2 card-color">
