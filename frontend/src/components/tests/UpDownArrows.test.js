b549d988328907ad9e74d850b30f096d1d28edda
brownben <9870007+brownben@users.noreply.github.com>
2020-08-15 20:10:36 +0100

    Test - UpDownArrows

---
+++ frontend/src/components/tests/UpDownArrows.test.js
@@ -0,0 +1,44 @@
+import { shallowMount } from '@vue/test-utils'
+import UpDownArrows from '@/components/UpDownArrows.vue'
+
+test('Renders Correctly', () => {
+  const wrapper = shallowMount(UpDownArrows)
+  expect(wrapper.findAll('svg').length).toBe(2)
+})
+
+test('Ascending', () => {
+  const wrapper = shallowMount(UpDownArrows, {
+    props: {
+      active: true,
+      ascending: true,
+    },
+  })
+  expect(wrapper.findAll('svg')[0].classes()).toContain(...['h-5', '-mt-1'])
+  expect(wrapper.findAll('svg')[1].classes()).toContain(...['h-2', 'mt-2'])
+})
+
+test('Not Ascending', () => {
+  const wrapper = shallowMount(UpDownArrows, {
+    props: {
+      active: true,
+      ascending: false,
+    },
+  })
+  expect(wrapper.findAll('svg')[0].classes()).toContain(...['h-2', 'mt-2'])
+  expect(wrapper.findAll('svg')[1].classes()).toContain(...['h-5', '-mt-1'])
+})
+
+test('Not Active', () => {
+  const wrapper = shallowMount(UpDownArrows, {
+    props: {
+      active: false,
+      ascending: true,
+    },
+  })
+  expect(wrapper.findAll('svg')[0].classes()).toContain(
+    ...['h-7/2', '-mt-1', 'text-opacity-75']
+  )
+  expect(wrapper.findAll('svg')[1].classes()).toContain(
+    ...['h-7/2', '-mt-1', 'text-opacity-75']
+  )
+})
