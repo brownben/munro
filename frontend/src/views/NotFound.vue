586b80f1b6f374e2d26719ce75ce0da8ced2bc4d
brownben <9870007+brownben@users.noreply.github.com>
2020-08-28 10:54:10 +0100

    Style - Remove Double Layout Not Found

--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -5,7 +5,7 @@
 -->
 
 <template>
-  <Layout>
+  <div class="w-full col-span-2 px-6 text-center">
     <Meta
       :head="{
         meta: { name: 'robots', content: 'noindex' },
@@ -14,38 +14,34 @@
       description="Page Not Found on Munro - League Results. Sorted. Sports League Results Calculated Quick and Easily, with Results Sorting and Filtering Options"
     />
 
-    <div class="w-full col-span-2 text-center">
-      <div class="inline-block w-full mt-8 md:w-2/3 xl:w-7/12">
-        <img
-          src="/@/assets/images/UndrawLost.svg"
-          class="object-contain w-full mx-auto max-h-112"
-          alt="Person Lost looking at Map"
-        />
-      </div>
-      <h1
-        class="mt-8 text-4xl font-bold text-gray-900 md:mt-12 md:text-5xl leading-12"
-      >
-        Sorry, I'm Lost
-      </h1>
-      <h2 class="mt-6 text-lg text-gray-500 md:text-xl">
-        I can't currently find what you are looking for, the link might be
-        broken or the page may have moved
-      </h2>
-
-      <div class="flex flex-row items-center justify-center mt-12">
-        <SearchBox />
-      </div>
+    <div class="inline-block w-full mt-8 md:w-2/3 xl:w-7/12">
+      <img
+        src="/@/assets/images/UndrawLost.svg"
+        class="object-contain w-full mx-auto max-h-112"
+        alt="Person Lost looking at Map"
+      />
     </div>
-  </Layout>
+    <h1
+      class="mt-8 text-4xl font-bold text-gray-900 md:mt-12 md:text-5xl leading-12"
+    >
+      Sorry, I'm Lost
+    </h1>
+    <h2 class="mt-6 text-lg text-gray-500 md:text-xl">
+      I can't currently find what you are looking for, the link might be broken
+      or the page may have moved
+    </h2>
+
+    <div class="flex flex-row items-center justify-center mt-12">
+      <SearchBox />
+    </div>
+  </div>
 </template>
 
 <script>
-import Layout from '/@/components/Layout.vue'
 import SearchBox from '/@/components/inputs/SearchBox.vue'
 
 export default {
   components: {
-    Layout,
     SearchBox,
   },
 }
