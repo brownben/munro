e5fd96b04df40f48a48b194374e109994b715224
brownben <9870007+brownben@users.noreply.github.com>
2020-09-02 19:49:44 +0100

    Feature - Top 3 Time Relative Scoring Adjusted by Age Class

--- backend/points/timeFromTop.py
+++ backend/points/timeFromTop.py
@@ -1,9 +1,12 @@
 from .helpers import *
+from .courseMultipliers import getMultiplier
 
 
 def assignPoints(data, leagueScoringMethod):
     if leagueScoringMethod == "timeTop3":
         return timeFromTop3(data)
+    elif leagueScoringMethod == "timeTop3Adjusted":
+        return timeFromTop3Adjusted(data)
     else:
         return False
 
@@ -23,7 +26,29 @@ def timeFromTop3(data, averagePoints=1000, standardDeviationPoints=200):
     ]
 
 
+def timeFromTop3Adjusted(data, averagePoints=1000, standardDeviationPoints=200):
+    courseStats = calculateCourseTop3Average(data)
+
+    return [
+        {
+            **result,
+            "points": timeFromTop3CalculatePointsAdjusted(
+                result,
+                courseStats.get(result["course"], 0),
+            ),
+        }
+        for result in data
+    ]
+
+
 def timeFromTop3CalculatePoints(result, average):
     points = (average / result["time"]) * 1000
+
+    return round(points)
+
+
+def timeFromTop3CalculatePointsAdjusted(result, average):
+    multiplier = getMultiplier(result["ageClass"], result["course"])
+    points = (average / result["time"]) * multiplier
 
     return round(points)
