ee39b377fee7c235e33697cbc2fcb1fefa51d953
brownben <9870007+brownben@users.noreply.github.com>
2020-09-13 20:32:43 +0100

    Refactor - UploadInstructions + Developers to Typegraphy Plugin
    - Move Page stylings to tailwind typography plugin to make more consistant
    - Remove most in component styling

--- frontend/src/components/cards/NoResultsCard.vue
+++ frontend/src/components/cards/NoResultsCard.vue
@@ -29,8 +29,3 @@ export default {
   },
 }
 </script>
-<style lang="postcss" scoped>
-img {
-  max-height: 14rem;
-}
-</style>
--- frontend/src/views/UploadInstructions.vue
+++ frontend/src/views/UploadInstructions.vue
@@ -2,7 +2,7 @@
   Instructions for Uploading results
 -->
 <template>
-  <Layout title="Upload Instructions" footer>
+  <Layout footer>
     <Meta
       :head="{
         meta: { name: 'robots', content: 'all' },
@@ -11,107 +11,105 @@
       description="Instructions for how to Upload Results to Munro - League Results. Sorted. Sports League Results Calculated Quick and Easily, with Results Sorting and Filtering Options"
       url="https://munro-leagues.herokuapp.com/upload/instructions"
     />
-    <InstructionCard :number="1">
-      <template #title>
-        Go to
-        <router-link to="/upload/file" class="link text-main-700">
-          https://munro-leagues.herokuapp.com/upload/file
-        </router-link>
-      </template>
-      <template #default>
-        <img
-          src="/@/assets/images/UploadInstructions-Blank.png"
-          alt="Empty Upload Form"
-        />
-      </template>
-    </InstructionCard>
-
-    <InstructionCard
-      :number="2"
-      title="Enter the Event ID and the 20 digit Upload Key Provided"
+    <article
+      class="col-span-2 pb-4 w-full mx-auto mt-4 mb-auto prose lg:pb-8 lg:prose-lg"
     >
-      <p>The event name should now be displayed.</p>
+      <h1>Upload Instructions</h1>
+      <ol>
+        <li>
+          <strong
+            >Go to
+            <router-link to="/upload/file" class="link text-main-700">
+              https://munro-leagues.herokuapp.com/upload/file
+            </router-link></strong
+          >
+          <img
+            src="/@/assets/images/UploadInstructions-Blank.png"
+            alt="Empty Upload Form"
+          />
+        </li>
+        <li>
+          <strong
+            >Enter the Event ID and the 20 digit Upload Key Provided</strong
+          >
+          <p>The event name should now be displayed.</p>
+          <p>
+            The Event ID will be the the form LeagueNameEventNameYYYY-MM-DD with
+            all spaces removed.
+          </p>
+          <p>
+            You can get the Event ID and Upload Key from your league coordinator
+            or by emailing munro.leagues@gmail.com
+          </p>
+          <img
+            src="/@/assets/images/UploadInstructions-EventDetails.png"
+            alt="Upload Form with Event ID and Upload Key Included"
+          />
+        </li>
+        <li>
+          <strong>Select Your Results File</strong>
+          <p>
+            Munro accepts any CSV results file in a sensible format, it has been
+            tested with OE CSV files and SITiming Processable CSV files.
+          </p>
+          <p>
+            It can be semicolon or comma delimited and should have the following
+            headings (they can be in any order):
+          </p>
+          <pre><code>Name; Age Class; Club; Course; Time; Position; NonCompetitive; Status;</code></pre>
+          <p>
+            Age Class, Club, Position and NonCompetitive are optional, but are
+            highly recommended to make sure the matching is done correctly
+          </p>
+        </li>
+        <li>
+          <strong>Enter Other Results Information</strong>
+          <p>
+            Enter the URLs of the Results Uploaded on Other Websites eg. HTML
+            Results/ Routegadget/ Winsplits.
+          </p>
+          <p>
+            If you forget, or need to change these URLs please contact your
+            league admin or munro.leagues@gmail.com who can update them
+          </p>
+          <img
+            src="/@/assets/images/UploadInstructions-Complete.png"
+            alt="Upload Form with Completeted with Upload Button Showing"
+          />
+        </li>
+        <li>
+          <strong>Confirm if Replacing Results</strong>
+          <p>
+            If results already exist a 'Overwrite Existing Results' checkbox
+            will appear, to confirm you wish to replace the exisiting results,
+            tick the box
+          </p>
+        </li>
+        <li>
+          <strong>Click `Upload File`</strong>
+          <p>
+            A message should appear saying that the data has been sent. Then a
+            message saying that the results upload is successful should appear
+            and you will be redirected to the results table
+          </p>
+        </li>
+      </ol>
+      <hr />
       <p>
-        The Event ID will be the the form LeagueNameEventNameYYYY-MM-DD with all
-        spaces removed.
-      </p>
-      <p>
-        You can get the Event ID and Upload Key from your league coordinator or
-        by emailing munro.leagues@gmail.com
-      </p>
-      <img
-        src="/@/assets/images/UploadInstructions-EventDetails.png"
-        alt="Upload Form with Event ID and Upload Key Included"
-      />
-    </InstructionCard>
-
-    <InstructionCard :number="3" title="Select Your Results File">
-      <p>
-        Munro accepts any CSV results file in a sensible format, it has been
-        tested with OE CSV files and SITiming Processable CSV files.
-      </p>
-      <p>
-        It can be semicolon or comma delimited and should have the following
-        headings (they can be in any order):
-      </p>
-      <code>
-        Name; Age Class; Club; Course; Time; Position; NonCompetitive; Status;
-      </code>
-      <p>
-        Age Class, Club, Position and NonCompetitive are optional, but are
-        highly recommended to make sure the matching is done correctly
-      </p>
-    </InstructionCard>
-
-    <InstructionCard :number="4" title="Enter Other Results Information">
-      <p>
-        Enter the URLs of the Results Uploaded on Other Websites eg. HTML
-        Results/ Routegadget/ Winsplits.
-      </p>
-      <p>
-        If you forget, or need to change these URLs please contact your league
-        admin or munro.leagues@gmail.com who can update them
-      </p>
-      <img
-        src="/@/assets/images/UploadInstructions-Complete.png"
-        alt="Upload Form with Completeted with Upload Button Showing"
-      />
-    </InstructionCard>
-
-    <InstructionCard :number="5" title=" Confirm if Replacing Results">
-      <p>
-        If results already exist a 'Overwrite Existing Results' checkbox will
-        appear, to confirm you wish to replace the exisiting results, tick the
-        box
-      </p>
-    </InstructionCard>
-
-    <InstructionCard :number="6" title="Click `Upload File`">
-      <p>
-        A message should appear saying that the data has been sent. Then a
-        message saying that the results upload is successful should appear and
-        you will be redirected to the results table
-      </p>
-    </InstructionCard>
-
-    <div class="col-span-2 card card-color">
-      <p class="p-1">
         If you need any help or wish to upload results in a different format
         please contact me at:
-        <span class="text-lg font-heading">munro.leagues@gmail.com</span>
+        <strong>munro.leagues@gmail.com</strong>
       </p>
-    </div>
+    </article>
   </Layout>
 </template>
 
 <script>
 import Layout from '/@/components/Layout.vue'
-import InstructionCard from '/@/components/cards/InstructionCard.vue'
 
 export default {
   components: {
     Layout,
-    InstructionCard,
   },
 }
 </script>
