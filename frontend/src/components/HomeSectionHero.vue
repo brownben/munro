6783f5bd87047043a3d5f6688f9f105aa8c4bc66
brownben <9870007+brownben@users.noreply.github.com>
2021-03-31 18:37:06 +0100

    revert: hero image as svg image as broke in build

--- frontend/src/components/HomeSectionHero.vue
+++ frontend/src/components/HomeSectionHero.vue
@@ -6,9 +6,11 @@
       <div
         class="w-full py-4 mx-auto mb-8 select-none md:m-0 sm:mb-10 sm:w-3/4 md:w-1/2"
       >
-        <div class="w-full">
-          <Image />
-        </div>
+        <img
+          src="../assets/images/RunnerLeagueTable.svg"
+          alt="Man Running In Front of Hills, with League Table In Front Of Him"
+          class="w-full"
+        />
       </div>
 
       <div>
@@ -27,6 +29,3 @@
     </section>
   </div>
 </template>
-<script lang="ts" setup>
-import Image from '../assets/images/RunnerLeagueTable.svg'
-</script>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import svgLoader from 'vite-svg-loader'
 
 export default defineConfig({
-  plugins: [vue(), svgLoader()],
+  plugins: [vue()],
   base: '/',
 
   server: {
