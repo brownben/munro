24dedcbcbdcfac3d026b380ea6e94d276b3a8bda
brownben <9870007+brownben@users.noreply.github.com>
2021-04-15 21:13:56 +0100

    feat: use headlessui for menu

--- frontend/src/components/AppMenu.vue
+++ frontend/src/components/AppMenu.vue
@@ -1,165 +1,109 @@
 <template>
-  <nav class="bg-white font-heading print:hidden">
-    <div class="max-w-screen-xl px-2 mx-auto sm:px-6 lg:px-8">
-      <div class="relative z-40 flex items-center justify-between h-16">
-        <div
-          class="flex items-center justify-center flex-1 sm:items-stretch sm:justify-between"
-        >
-          <div class="flex-shrink-0">
-            <router-link
-              to="/"
-              class="flex flex-row items-center"
-              aria-label="Home"
-              @click="menuOpen = false"
-            >
-              <svg
-                viewBox="0 0 301 260"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                class="inline-block w-10 h-8 sm:w-20 md:w-10"
-                alt="Munro Logo (Mountains Shaped as M)"
-                aria-hidden="true"
-              >
-                <path
-                  d="M300.058 259.827L260.933 0L89.3914 259.82L300.058 259.827Z"
-                  fill="currentColor"
-                  class="text-main-600"
-                />
-                <path
-                  d="M0.107626 259.827C-2.37174 256.108 38.7722 1.93888 42.4875 3.80219C46.2027 5.66549 200.744 259.827 200.744 259.827H0.107626Z"
-                  fill="currentColor"
-                  class="text-main-500"
-                />
-              </svg>
-              <h1
-                class="hidden ml-1 -mb-4 text-3xl leading-7 md:inline-block font-heading text-main-600"
-              >
-                <span class="sr-only">M</span>unro
-              </h1>
-            </router-link>
-          </div>
-          <div class="hidden sm:block sm:ml-6">
-            <div class="flex">
+  <Menu>
+    <nav class="bg-white font-heading print:hidden">
+      <div class="max-w-screen-xl px-2 mx-auto sm:px-6 lg:px-8">
+        <div class="relative z-40 flex items-center justify-between h-16">
+          <div
+            class="flex items-center justify-center flex-1 sm:items-stretch sm:justify-between"
+          >
+            <div class="flex-shrink-0">
               <router-link
-                v-for="link of links"
-                :key="link?.location"
-                :to="link?.location"
-                class="px-3 py-2 ml-4 font-medium leading-5 transition duration-150 ease-in-out rounded-shape focus:outline-none"
-                :class="
-                  $route.path === link?.location
-                    ? 'text-main-700 bg-main-100'
-                    : 'hover:bg-main-100 hover:text-main-700 focus:bg-main-100 focus:text-main-700 text-gray-500'
-                "
+                to="/"
+                class="flex flex-row items-center"
+                aria-label="Home"
               >
-                {{ link?.text }}
-              </router-link>
-              <router-link
-                v-if="auth.loggedIn"
-                to="/logout"
-                class="px-3 py-2 ml-4 font-medium leading-5 text-gray-500 transition duration-150 ease-in-out rounded-shape focus:outline-none hover:bg-main-100 hover:text-main-700 focus:bg-main-100 focus:text-main-700"
-              >
-                Logout
+                <MunroLogo class="inline-block w-10 h-8 sm:w-20 md:w-10" />
+                <h1
+                  class="hidden ml-1 -mb-4 text-3xl leading-7 md:inline-block font-heading text-main-600"
+                >
+                  <span class="sr-only">M</span>unro
+                </h1>
               </router-link>
             </div>
+            <div class="hidden sm:block sm:ml-6">
+              <div class="flex">
+                <router-link
+                  v-for="link of links"
+                  v-slot="{ isActive }"
+                  :key="link?.location"
+                  :to="link?.location"
+                  class="px-3 py-2 ml-4 font-medium leading-5 text-gray-500 transition duration-150 ease-in-out rounded-shape hover:bg-main-100 hover:text-main-700 focus:bg-main-100 focus:text-main-700 focus-visible:shadow-outline"
+                  active-class="text-main-700 bg-main-100"
+                >
+                  {{ link?.text }}
+                </router-link>
+                <router-link
+                  v-if="auth.loggedIn"
+                  to="/logout"
+                  class="px-3 py-2 ml-4 font-medium leading-5 text-gray-500 transition duration-150 ease-in-out rounded-shape hover:bg-main-100 hover:text-main-700 focus:bg-main-100 focus:text-main-700 focus-visible:shadow-outline"
+                >
+                  Logout
+                </router-link>
+              </div>
+            </div>
+          </div>
+
+          <div class="absolute inset-y-0 right-0 flex items-center sm:hidden">
+            <MenuButton
+              v-slot="{ open }"
+              class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-shape hover:text-main-700 hover:bg-main-100 focus:outline-none focus:bg-main-100 focus:text-main-700"
+            >
+              <span class="sr-only">Open main menu</span>
+
+              <HamburgerMenuIcon v-if="!open" class="w-6 h-6" />
+              <CloseIcon v-else class="w-6 h-6" />
+            </MenuButton>
           </div>
         </div>
-        <div class="absolute inset-y-0 right-0 flex items-center sm:hidden">
-          <!-- Mobile menu button-->
-          <button
-            class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-shape hover:text-main-700 hover:bg-main-100 focus:outline-none focus:bg-main-100 focus:text-main-700"
-            aria-label="Main menu"
-            :aria-expanded="menuOpen"
-            @click="menuOpen = !menuOpen"
+        <transition
+          enter-active-class="motion-safe:transition duration-75 ease-out origin-top motion-safe:transform"
+          enter-from-class="scale-y-90 opacity-75"
+          enter-to-class="scale-y-100 opacity-100"
+          leave-active-class="motion-safe:transition duration-75 ease-in origin-top motion-safe:transform"
+          leave-from-class="scale-y-100 opacity-100"
+          leave-to-class="scale-y-90 opacity-75"
+        >
+          <MenuItems
+            class="absolute z-30 flex flex-col justify-center w-full pt-16 -mt-16 bg-white shadow sm:hidden outline-none left-0"
           >
-            <!-- Icon when menu is closed. -->
-            <!-- Menu open: "hidden", Menu closed: "block" -->
-            <svg
-              :class="menuOpen ? 'hidden' : 'block'"
-              class="w-6 h-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              role="img"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-            <!-- Icon when menu is open. -->
-            <!-- Menu open: "block", Menu closed: "hidden" -->
-            <svg
-              :class="menuOpen ? 'block' : 'hidden'"
-              class="w-6 h-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              role="img"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
-        </div>
+            <div class="px-2 pt-2 pb-3 text-left">
+              <MenuItem
+                v-for="link of links"
+                :key="link?.location"
+                v-slot="{ active }"
+              >
+                <router-link
+                  :to="link?.location"
+                  class="block px-3 py-2 mt-1 text-lg font-medium text-gray-500 transition duration-150 ease-in-out rounded-shape focus:outline-none hover:bg-main-100 hover:text-main-700 focus:bg-main-100 focus:text-main-700"
+                  :class="active && 'text-main-600 bg-main-100'"
+                  active-class="text-main-700 bg-main-100"
+                >
+                  {{ link?.text }}
+                </router-link>
+              </MenuItem>
+              <MenuItem v-if="auth.loggedIn">
+                <router-link
+                  to="/logout"
+                  class="block px-3 py-2 mt-1 text-lg font-medium text-gray-500 transition duration-150 ease-in-out rounded-shape focus:outline-none hover:bg-main-100 hover:text-main-700 focus:bg-main-100 focus:text-main-700"
+                >
+                  Logout
+                </router-link>
+              </MenuItem>
+            </div>
+          </MenuItems>
+        </transition>
       </div>
-    </div>
-
-    <!--
-    Mobile menu, toggle classes based on menu state.
-
-    Menu open: "block", Menu closed: "hidden"
-  -->
-    <transition
-      enter-active-class="motion-safe:transition duration-100 ease-out origin-top motion-safe:transform"
-      enter-from-class="scale-y-90 opacity-75"
-      enter-to-class="scale-y-100 opacity-100"
-      leave-active-class="motion-safe:transition duration-75 ease-in origin-top motion-safe:transform"
-      leave-from-class="scale-y-100 opacity-100"
-      leave-to-class="scale-y-90 opacity-75"
-    >
-      <div
-        v-show="menuOpen"
-        class="absolute z-30 flex flex-col justify-center w-full pt-16 -mt-16 bg-white shadow sm:hidden"
-      >
-        <div class="px-2 pt-2 pb-3 text-left">
-          <router-link
-            v-for="link of links"
-            :key="link?.location"
-            :to="link?.location"
-            class="block px-3 py-2 mt-1 text-lg font-medium text-gray-500 transition duration-150 ease-in-out rounded-shape focus:outline-none hover:bg-main-100 hover:text-main-700 focus:bg-main-100 focus:text-main-700"
-            :class="
-              $route.path === link?.location
-                ? 'text-main-600 bg-main-100'
-                : null
-            "
-            @click="menuOpen = false"
-          >
-            {{ link?.text }}
-          </router-link>
-          <router-link
-            v-if="auth.loggedIn"
-            to="/logout"
-            class="block px-3 py-2 mt-1 text-lg font-medium text-gray-500 transition duration-150 ease-in-out rounded-shape focus:outline-none hover:bg-main-100 hover:text-main-700 focus:bg-main-100 focus:text-main-700"
-            @click="menuOpen = false"
-          >
-            Logout
-          </router-link>
-        </div>
-      </div>
-    </transition>
-  </nav>
+    </nav>
+  </Menu>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-
+import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
 import { useAuthentication } from '../store/authentication'
+
+import MunroLogo from './images/MunroLogo.vue'
+import HamburgerMenuIcon from './images/HamburgerMenu.vue'
+import CloseIcon from './images/Close.vue'
 
 const auth = useAuthentication()
 
@@ -168,7 +112,6 @@ interface Link {
   location: string
 }
 
-const menuOpen = ref(false)
 const links: Link[] = [
   { text: 'Leagues', location: '/leagues' },
   { text: 'Latest Results', location: '/latest-results' },
---
+++ frontend/src/components/images/Close.vue
@@ -0,0 +1,10 @@
+<template>
+  <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M6 18L18 6M6 6l12 12"
+    />
+  </svg>
+</template>
