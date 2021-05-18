52dd1ceafe6e78a036ea2cd71dd7b0c66e7bdfba
brownben <9870007+brownben@users.noreply.github.com>
2021-05-18 16:36:34 +0100

    fix: positioning of messages

--- frontend/src/components/AppMessages.vue
+++ frontend/src/components/AppMessages.vue
@@ -1,25 +1,26 @@
 <template>
-  <transition name="fade">
-    <transition-group
-      v-show="messages.all.length > 0"
-      id="messages"
-      tag="div"
-      name="messages"
-      mode="out-in"
-      role="alert"
-      class="fixed bottom-0 right-0 z-50 mx-4 my-4"
-    >
-      <template v-for="message of messages.all" :key="message.id">
-        <p
-          v-if="message.visible"
-          class="mt-4 text-lg select-none card card-color font-heading"
-          @click="clear(message.id)"
-        >
-          {{ message.text }}
-        </p>
-      </template>
-    </transition-group>
-  </transition>
+  <div class="fixed bottom-0 right-0 z-50 mx-4 my-4">
+    <transition name="fade">
+      <transition-group
+        v-show="messages.all.length > 0"
+        tag="div"
+        id="messages"
+        name="messages"
+        mode="out-in"
+        role="alert"
+      >
+        <template v-for="message of messages.all" :key="message.id">
+          <p
+            v-if="message.visible"
+            class="mt-4 text-lg select-none card card-color font-heading"
+            @click="clear(message.id)"
+          >
+            {{ message.text }}
+          </p>
+        </template>
+      </transition-group>
+    </transition>
+  </div>
 </template>
 
 <script setup lang="ts">
