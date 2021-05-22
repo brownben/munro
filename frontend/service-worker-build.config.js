85c7408f1e9c4f2937ab4f1e58af72c61ecca905
brownben <9870007+brownben@users.noreply.github.com>
2021-05-22 22:07:44 +0100

    fix: replace NODE_ENV

--- frontend/service-worker-build.config.js
+++ frontend/service-worker-build.config.js
@@ -1,4 +1,6 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+import replace from '@rollup/plugin-replace'
 
 export default {
   input: 'src/service-worker.js',
@@ -6,5 +8,12 @@ export default {
     file: 'dist/service-worker.js',
     format: 'esm',
   },
-  plugins: [nodeResolve()],
+  plugins: [
+    nodeResolve(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      preventAssignment: true,
+    }),
+    terser(),
+  ],
 }
