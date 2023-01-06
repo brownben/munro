8a0aad00c2dbfc437be52a3fd81311491dca9ea1
Ben Brown <9870007+brownben@users.noreply.github.com>
2023-01-06 12:22:27 +0000

    fix: close mobile menu on navigation

--- frontend/components/Menu.vue
+++ frontend/components/Menu.vue
@@ -98,13 +98,14 @@ const links: Link[] = [
               <MenuItem
                 v-for="link of links"
                 :key="link.location"
-                v-slot="{ active }"
+                v-slot="{ active, close }"
               >
                 <NuxtLink
                   :to="link.location"
                   class="mt-1 block rounded-md px-3 py-2 font-normal text-gray-500 ring-main-200 transition duration-150 ease-in-out hover:bg-main-100 hover:text-main-700 focus:bg-main-100 focus:text-main-700 focus:outline-none focus-visible:ring"
                   :class="active && 'bg-main-100 text-main-700'"
                   active-class="text-main-700 bg-main-100"
+                  @click="close"
                 >
                   {{ link.text }}
                 </NuxtLink>
