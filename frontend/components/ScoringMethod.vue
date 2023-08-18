36a6e01bfc9b81d64bacaf32e6f0e4d77e584afb
Ben Brown <9870007+brownben@users.noreply.github.com>
2023-08-18 11:18:15 +0100

    feat: scoring method page

---
+++ frontend/components/ScoringMethod.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+defineProps({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  examplePositions: { type: Array as PropType<string[]>, required: true },
+  examplePoints: { type: Array as PropType<string[]>, required: true },
+  exampleTimes: { type: Array as PropType<string[]>, default: () => [] },
+})
+</script>
+<template>
+  <article id="position-average" class="grid gap-8 sm:gap-12 sm:grid-cols-5">
+    <div class="col-span-3">
+      <h2 class="font-bold text-xl sm:text-2xl">
+        {{ name }}
+      </h2>
+      <p class="text-gray-600 mt-2">
+        {{ description }}
+      </p>
+    </div>
+    <table class="text-left col-span-2">
+      <tr class="border-b border-main-400">
+        <th class="font-medium px-4 py-1">Position</th>
+        <th v-if="exampleTimes.length" class="font-medium px-4 py-1">Time</th>
+        <th class="font-medium px-4 py-1">Points</th>
+      </tr>
+      <tr
+        v-for="(position, index) in examplePositions"
+        :key="index"
+        :class="{ 'bg-main-50': index % 2 == 1 }"
+      >
+        <td class="px-4 py-1 ordinal">{{ position }}</td>
+        <td v-if="exampleTimes.length" class="px-4 py-1">
+          {{ exampleTimes[index] }}
+        </td>
+        <td class="px-4 py-1">{{ examplePoints[index] }}</td>
+      </tr>
+    </table>
+  </article>
+</template>
---
+++ frontend/pages/scoring-methods.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+useTitle({
+  title: 'Scoring Methods',
+  description: 'How the different scoring methods work.',
+})
+</script>
+
+<template>
+  <div>
+    <Heading
+      title="Scoring Methods"
+      tagline="How the different scoring methods work."
+    />
+
+    <main
+      class="mx-auto flex max-w-screen-lg flex-col gap-12 px-6 py-10 sm:grid sm:pt-10 lg:px-8"
+    >
+      <ScoringMethod
+        id="position"
+        name="Position Based Scoring"
+        description="Points are assigned based on your position in the course. The winner
+            gets the most, counting down by one for each place behind the
+            winner. No one gets less than 0 points. Can start from different
+            values (100, 99, 50)."
+        :example-positions="['1st', '2nd', '3rd', '...', '99th']"
+        :example-points="['100', '99', '98', '...', '1']"
+      />
+
+      <ScoringMethod
+        id="position-average"
+        name="Position Based Scoring (Reduced In Draw)"
+        description="Points are assigned based on your position in the course. The winner
+            gets the most, counting down by one for each place behind the
+            winner. No one gets less than 0 points. If there is a draw the
+            points assigned is the average across all positions covered."
+        :example-positions="['1st', '=2nd', '=2nd', '4th', '...']"
+        :example-points="['100', '98.5', '98.5', '97', '...']"
+      />
+
+      <ScoringMethod
+        id="position-staggered"
+        name="Position Based Scoring (Staggered)"
+        description="Points are assigned based on your position in the course. The winner
+            gets the most, with the first 5 places getting having larger gaps
+            before counting down by 1 for each place. No one gets less than 0
+            points."
+        :example-positions="['1st', '2nd', '3rd', '4th', '5th', '6th']"
+        :example-points="['60', '55', '51', '48', '46', '45']"
+      />
+
+      <ScoringMethod
+        id="time-average"
+        name="Time Relative to Average"
+        description="The average time on the course is given 1000 points. If you are faster by one standard deviation of the times, you get 200 more points, and 200 less if you are slower by one standard deviation. Used for the Scottish Orienteering League. Can also have a base of 100 points."
+        :example-positions="['1st', '2nd', '3rd', '4th', '5th']"
+        :example-times="['10', '12', '15', '25', '65']"
+        :example-points="['1135', '1117', '1091', '1003', '654']"
+      />
+
+      <ScoringMethod
+        id="time-top"
+        name="Time Relative to Top 3"
+        description="The average time of the first 3 competitors on the course gets 1000 points. You are then assigned points based on what proportion of that average time was yours."
+        :example-positions="['1st', '2nd', '3rd', '4th', '5th', '6th']"
+        :example-times="['45', '50', '55', '62', '100', '150']"
+        :example-points="['1111', '1000', '909', '806', '500', '330']"
+      />
+    </main>
+  </div>
+</template>
