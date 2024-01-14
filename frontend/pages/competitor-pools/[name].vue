0ffab34fabadfd21d6eb70f95025b0057993083e
Ben Brown <9870007+brownben@users.noreply.github.com>
2024-01-14 16:47:57 +0000

    fix: rename button

--- frontend/pages/competitor-pools/[name].vue
+++ frontend/pages/competitor-pools/[name].vue
@@ -55,7 +55,7 @@ useTitle({
             class="-ml-1 mr-2 h-5 w-5"
             aria-hidden="true"
           />
-          <span>Filter Results</span>
+          <span>Filter Competitors</span>
         </button>
       </template>
     </Heading>
